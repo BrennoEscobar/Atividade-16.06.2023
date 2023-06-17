63ca018f206e94baac753af1c5407a95120a8019
BrennoEscobar <106408359+BrennoEscobar@users.noreply.github.com>
2023-06-16 21:07:40 -0300

    Add files via upload

---
+++ Atividade do dia 16-06-2023/script.js
@@ -0,0 +1,13 @@
+function titulo(){
+    var titulo = window.document.getElementById('titulo')
+    var resp_ti = prompt('insira o titulo: ')
+    titulo.innerText = resp_ti
+}
+titulo()
+
+function subtitulo(){
+    var subtitulo = window.document.getElementById('subtitulo')
+    var resp_ti1 = prompt('insira o subtitulo: ')
+    subtitulo.innerText = resp_ti1
+}
+subtitulo()
